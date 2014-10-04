c7e484dfeb3961eea93162b1428bf4323f538600
Kristijan <kica993@gmail.com>
2014-10-04 19:02:53 +0200

    Added Log In Frame

--- FSMS/FSMS/Form1.Designer.cs
+++ FSMS/FSMS/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace FSMS
 {
-    partial class Form1
+    partial class LogIn
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.tbUsername = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnLogIn
+            // 
+            this.btnLogIn.AutoSize = true;
+            this.btnLogIn.Location = new System.Drawing.Point(210, 174);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(75, 30);
+            this.btnLogIn.TabIndex = 0;
+            this.btnLogIn.Text = "Log In";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            // 
+            // tbUsername
+            // 
+            this.tbUsername.AccessibleName = "";
+            this.tbUsername.Location = new System.Drawing.Point(155, 63);
+            this.tbUsername.Name = "tbUsername";
+            this.tbUsername.Size = new System.Drawing.Size(189, 26);
+            this.tbUsername.TabIndex = 1;
+            this.tbUsername.Tag = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 119);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(189, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(151, 40);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(83, 20);
+            this.lblUsername.TabIndex = 3;
+            this.lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(155, 96);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(78, 20);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Password";
+            // 
+            // LogIn
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(688, 835);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(497, 304);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.tbUsername);
+            this.Controls.Add(this.btnLogIn);
+            this.Name = "LogIn";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Log In";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLogIn;
+        private System.Windows.Forms.TextBox tbUsername;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lblPassword;
     }
 }
 
--- FSMS/FSMS/Program.cs
+++ FSMS/FSMS/Program.cs
@@ -15,7 +15,7 @@ namespace FSMS
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LogIn());
         }
     }
 }
